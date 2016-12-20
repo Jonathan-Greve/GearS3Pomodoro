8b081a83c530e1144dd0e2aded260272aaaf2521
Jonathan Bjørn Greve <jonagrev@gmail.com>
2016-12-20 21:07:42 +0100

    Added countdown functionality
    Uses the setInterval and clearInterval methods, when started and paused
    resepctively.

--- app.js
+++ app.js
@@ -16,10 +16,9 @@
 
 
 var TEXT_START = "Start";
-var TEXT_STOP = "Stop";
-var SEC;
-var MIN;
-var HOUR;
+var TEXT_PAUSE = "Pause";
+var SEC = 25*60;
+var timer;
 
 var timerStartBtn;
 var resetBtn
@@ -33,7 +32,7 @@ function init() {
 
     timerStartBtn = document.getElementById('timer-start-btn');
     resetBtn = document.getElementById('btn-reset');
-    
+
     timeSec = document.getElementById('text-main-second');
     timeMin = document.getElementById('text-main-minute');
     timeHour = document.getElementById('text-main-hour');
@@ -41,40 +40,56 @@ function init() {
 
     timerStartBtn.addEventListener("click", onTimerStartBtnClick);
     resetBtn.addEventListener("click", resetTimer);
-    
 }
 
 function onTimerStartBtnClick() {
     console.log("onTimerStartBtnClick() called");
-    if (timerStartBtn.innerHTML === TEXT_START){
-    	console.log("info on button = start");
-    	startTimer();
+    if (timerStartBtn.innerHTML === TEXT_START) {
+        console.log("info on button = start");
+        startTimer();
     } else {
-    	console.log("info on button = stop");
-    	stopTimer();
+        console.log("info on button = pause");
+        stopTimer();
     }
 }
 
 function startTimer() {
-	console.log("startTimer() called");
-	timerStartBtn.innerHTML = TEXT_STOP;
-	timeSec.innerHTML = "59";
-	timeMin.innerHTML = "24";
-	timeHour.innerHTML = "00";
+    console.log("startTimer() called");
+    timerStartBtn.innerHTML = TEXT_PAUSE;
+    timer = setInterval(timerCount, 1000);
 }
 
 function stopTimer() {
-	console.log("stopTimer() called");
-	timerStartBtn.innerHTML = TEXT_START;
+    console.log("stopTimer() called");
+    timerStartBtn.innerHTML = TEXT_START;
+    clearInterval(timer);
 }
 
-function resetTimer(){
-	console.log("resetTimer() called");
-	timeSec.innerHTML = "00";
-	timeMin.innerHTML = "25";
-	timeHour.innerHTML = "00";
+function resetTimer() {
+    console.log("resetTimer() called");
+    timeSec.innerHTML = "00";
+    timeMin.innerHTML = "25";
+    timeHour.innerHTML = "00";
+    SEC = 25*60;
 }
 
+function timerCount() {
+    SEC = SEC - 1;
 
+    timeFormatter(SEC);
+}
 
+function timeFormatter(_SEC) {
+    var totalSecond = _SEC
+    console.log("totalSecond" + totalSecond.toString());
+    var sec = totalSecond % 60;
+    console.log("sec" + sec.toString());
+    var min = Math.floor(((totalSecond - sec) / 60) % 60);
+    console.log("min" + min.toString());
+    var hour = (totalSecond - sec - 60 * min) / (3600);
+    console.log("hour" + hour.toString());
 
+    timeSec.innerHTML = (sec < 10) ? "0" + sec.toString() : sec.toString();
+    timeMin.innerHTML = (min < 10) ? "0" + min.toString() : min.toString();
+    timeHour.innerHTML = (hour < 10) ? "0" + hour.toString() : hour.toString();
+}
